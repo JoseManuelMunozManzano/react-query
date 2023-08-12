9aa9f06cf6f5c220d786d7b2c5ca1e6e31eeb4d6
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-08-12 08:10:27 +0200

    01-Mejorar experiencia de usuario - Botón

--- 01-cripto-random/README.md
+++ 01-cripto-random/README.md
@@ -2,6 +2,8 @@
 
 Mini-proyecto para ver las ventajas que tiene usar React Query.
 
+En este primer punto NO SE USA REACT QUERY, sino que se ve la cantidad de trabajar que hay que acometer para mantener estados y hacer funcionar la app.
+
 Se está usando esta API de ruido atmosférico: `https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new`
 
 Su valor lo mostraremos en pantalla.
@@ -21,9 +23,11 @@ Acceder al navegador, a la ruta indicada en VITE
 ## Cosas que se ven
 
 - React.StrictMode
-- UseEffect no puede ser asíncrono
+- useEffect no puede ser asíncrono
 - Uso de fetch API
 - Mejorar experiencia de usuario
 - Buenas prácticas de React con respecto a hacer independientes los useEffect
-- Uso de ternario
+- Uso de ternarios
 - Uso de throw y manejo de errores
+- Uso de botones y disabled para evitar pulsarlo mientras trae información
+- useReducer
--- 01-cripto-random/src/App.tsx
+++ 01-cripto-random/src/App.tsx
@@ -1,11 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import './App.css';
 
 const getRandomNumberFromApi = async (): Promise<number> => {
   const res = await fetch('https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new');
   const numberString = await res.text();
 
-  throw new Error('Auxilio!!!');
   return +numberString;
 };
 
@@ -15,13 +14,16 @@ export const App = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   // Si ocurre algún error se lo indicamos al usuario
   const [error, setError] = useState<string>();
+  // Tenemos un número que cambia, forzando al primer useEffect que se vuelva a disparar
+  const [key, forceRefetch] = useReducer((x: number) => x + 1, 0);
 
   // Como useEffect no puede ser asíncrono, usamos el .then
   useEffect(() => {
+    setIsLoading(true);
     getRandomNumberFromApi()
       .then(setNumber)
       .catch((error: Error) => setError(error.message));
-  }, []);
+  }, [key]);
 
   // Según las buenas prácticas de React, cuando se hacen modificaciones, acciones o efectos secundarios,
   // nuestros useEffect deben ser independientes
@@ -37,6 +39,9 @@ export const App = () => {
     <div className="App App-header">
       {isLoading ? <h2>Cargando...</h2> : <h2>Número aleatorio: {number}</h2>}
       {!isLoading && error && <h3>{error}</h3>}
+      <button onClick={forceRefetch} disabled={isLoading}>
+        {isLoading ? '...' : 'Nuevo número'}
+      </button>
     </div>
   );
 };
