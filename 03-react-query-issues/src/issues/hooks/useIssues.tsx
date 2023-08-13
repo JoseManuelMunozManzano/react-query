2c37cc35af7d423cc893ffc0bbccb0ec2500c34b
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-08-13 06:51:03 +0200

    04-Issues - Mostrar issues e issue items en pantalla

--- 03-react-query-issues/src/issues/hooks/useIssues.tsx
+++ 03-react-query-issues/src/issues/hooks/useIssues.tsx
@@ -4,6 +4,7 @@ import { Issue } from '../interfaces';
 
 const getIssues = async (): Promise<Issue[]> => {
   const { data } = await githubApi.get<Issue[]>('/issues');
+  console.log(data);
   return data;
 };
 
--- 03-react-query-issues/src/issues/interfaces/index.ts
+++ 03-react-query-issues/src/issues/interfaces/index.ts
@@ -1,3 +1,4 @@
 // Cuando estamos trabajando con Vite, es buena práctica indicar export type en los ficheros de barril cuando exportamos tipos
-export type { Issue } from './issue';
+// Truco cuando pasamos dos, es poner el type en uno de los dos
+export { type Issue, State } from './issue';
 export type { Label } from './label';
